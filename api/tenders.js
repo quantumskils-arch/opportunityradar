6fe92c2558fceec5f50d5dde9e89b9782ae8c23d
quantumskils-arch <quantumskils@gmail.com>
2026-05-25 13:54:33 +0200

    fix: Vercel proxy API for live tender data

---
+++ api/tenders.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  try {
+    const response = await fetch('http://187.77.117.32:8888/api/tenders');
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
